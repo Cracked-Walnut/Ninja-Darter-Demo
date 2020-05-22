30b06e6b3c306e43c34e1cdc7cdae947864b4eaf
Cracked-Walnut <maolud62@gmail.com>
2020-05-22 01:11:22 -0400

    More chunk optimization

---
+++ Square-Up!/Assets/Scripts/AI/EnemyShooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+    
+    private float timer;
+    private bool isTimerRunning;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    
+    void Start() {
+        timer = 1.5f;
+        isTimerRunning = true;
+    }
+
+    void Update() {
+        shoot();
+    }
+
+    private void shoot() {
+        if (isTimerRunning) {
+            timer -= timer.deltaTime;
+            if (timer == 0.0f) {
+                Instantiate(bulletPrefab, firePoint.position, 0);
+                timer = 1.5f;
+            }
+        }
+    }
+
+}
