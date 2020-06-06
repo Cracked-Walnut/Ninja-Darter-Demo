de6719aa5f625ca722037a38ab239eaf38fc77f0
Cracked-Walnut <maolud62@gmail.com>
2020-06-06 18:50:11 -0400

    Update SpawnObject.cs

--- Square-Up!/Assets/Scripts/Procedural/SpawnObject.cs
+++ Square-Up!/Assets/Scripts/Procedural/SpawnObject.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour {
 
+    private int rand;
+
     public GameObject[] objects;
+    private GameObject instance;
+    private RoomType roomType;
+
+    void Awake() {
+        roomType = FindObjectOfType<RoomType>();
+        rand = Random.Range(0, objects.Length);
+    }
 
     private void Start() {
         if (objects == null || objects.Length == 0)
             return;
-        else {
-            int rand = Random.Range(0, objects.Length);
-            GameObject instance = (GameObject) Instantiate(objects[rand], transform.position, Quaternion.identity);
-            instance.transform.parent = transform;
+        else
+            SpawnRoom();
         }
+
+    void SpawnRoom() {
+        int rand = Random.Range(0, objects.Length);
+        instance = (GameObject) Instantiate(objects[rand], transform.position, Quaternion.identity);
+        instance.transform.parent = transform;
     }
 }
+
 /*
 Sources:
 1) B., Blackthornprod, 'ROGUE LIKE RANDOM LEVEL GENERATION - INTERMEDIATE C#/UNITY TUTORIAL - #1', 2018. [Online]. Available: https://www.youtube.com/watch?v=hk6cUanSfXQ
