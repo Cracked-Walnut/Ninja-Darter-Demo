008214bd04696f6bfc7a469f6028ff50ab92ee17
Cracked-Walnut <maolud62@gmail.com>
2020-06-25 23:29:01 -0400

    Work commenced on Stage Selector buttons

---
+++ Square-Up!/Assets/Scripts/Menu/FinishStage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishStage : MonoBehaviour {
+
+    private SceneLoader sceneLoader;
+    private StageButton stageButton;
+
+    void Awake() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+        stageButton = FindObjectOfType<StageButton>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Player") {
+            sceneLoader.loadStage("Stage_Selector");
+            stageButton.IsButtonDisabled(false);
+        }
+	}
+}
---
+++ Square-Up!/Assets/Scripts/Menu/StageButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class StageButton : MonoBehaviour {
+
+    [SerializeField] private Button button;
+
+    void Start() {
+        IsButtonDisabled(true);
+    }
+
+    public void IsButtonDisabled(bool disabled) {
+        if (disabled)
+            button.interactable = false;
+        else
+            button.interactable = true;
+    }
+
+    public Button GetButton() {
+        return button;
+    }
+}
