bd0a569c22182fda65d06fbfc20a0a55c10fa741
Cracked-Walnut <maolud62@gmail.com>
2020-07-03 17:42:03 -0400

    Setting up a save and loading script for easy data retrieval

--- Square-Up!/Assets/Scripts/ItemDrops/ItemDropChance.cs
+++ Square-Up!/Assets/Scripts/ItemDrops/ItemDropChance.cs
@@ -6,7 +6,7 @@ public class ItemDropChance : MonoBehaviour {
     /*I'll dedicate a class for each item. Use inheritance to have items inherit generic properties*/
 
     private float randomDropChanceNum = 0.0f; 
-    private const float MIN_DROP_CHANCE = 1.0f, MAX_DROP_CHANCE = 100.0f;
+    private const float MIN_DROP_CHANCE = 1.0f, MAX_DROP_CHANCE = 15.0f;
 
     float generateDropChance(float minimumDropChance, float maximumDropChance) {
         randomDropChanceNum = Random.Range(minimumDropChance, maximumDropChance);
---
+++ Square-Up!/Assets/Scripts/SavingAndLoading/SerializeableData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializeableData {
+
+    // place the data you want saved here
+    // HP
+    // Ammo count
+    // Coins Collected
+    // All upgrades
+
+}
