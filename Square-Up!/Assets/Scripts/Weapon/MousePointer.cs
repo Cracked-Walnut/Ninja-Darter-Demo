596f9976747ccf06faa5a392ccb0628e9fac1b24
Cracked-Walnut <maolud62@gmail.com>
2020-07-06 00:25:57 -0400

    Working on mouse pointer, which will be used to aim the weapon

---
+++ Square-Up!/Assets/Scripts/Weapon/MousePointer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour {
+
+    [SerializeField] private Texture2D cursorArrow;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
+
+/*
+Sources:
+1) O. T., OXMOND Tutorials, 'How To Change The Cursor In Unity! [Unity3D 2019 Beginner Tutorial]', 2019. [Online]. Available: https://www.youtube.com/watch?v=W4SE0_cfAqc [Accessed: 05-Jul-2020].
+*/
