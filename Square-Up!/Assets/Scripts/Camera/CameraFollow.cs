b3a4389e5720603c2ef0272cf1d0778d04a07a14
Cracked-Walnut <maolud62@gmail.com>
2020-07-26 03:15:16 -0400

    Camera will now follow the player on X axis and snap to the appropriate chunk on Y axis

--- Square-Up!/Assets/Scripts/Camera/CameraFollow.cs
+++ Square-Up!/Assets/Scripts/Camera/CameraFollow.cs
@@ -8,15 +8,32 @@ public class CameraFollow : MonoBehaviour {
 	[SerializeField] private float smoothSpeed = 0.125f;
 	[SerializeField] private Vector3 offset;
 
-	void Update() {}
+	void Update() { }
 
-	void FixedUpdate () {
+	// void FixedUpdate () {
 
-		Vector3 desiredPosition = target.position + offset; //Adding x, y, z position of our target to our offset
-		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		transform.position = desiredPosition;
+	// 	Vector3 desiredPosition = target.position + offset; //Adding x, y, z position of our target to our offset
+	// 	Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+	// 	transform.position = desiredPosition;
+	// }
+
+	void FixedUpdate() {
+
+		if (target.transform.position.y > 200f && target.transform.position.y < 240f) {
+			transform.position = new Vector3(target.transform.position.x, 210f, -10f);
+		}
+		else if (target.transform.position.y < 200f && target.transform.position.y > 160f) {
+			transform.position = new Vector3(target.transform.position.x, 180f, -10f);
+		} else
+			transform.position = new Vector3(target.transform.position.x, 230f, -10f);
+	
+		// if(target.transform.position.y < -20f && target.transform.position.y > - 60f)
+		// 	transform.position = new Vector3(target.transform.position.x, -40f, -10f);
+
+		
 	}
 /*
+https://gamedev.stackexchange.com/questions/147526/only-make-camera-follow-player-on-x-axis
 1) B., Brackeys, 'Smooth Camera Follow in Unity - Tutorial', 2017. [Online]. Available: https://www.youtube.com/watch?v=MFQhpwc6cKE
 */
 
