451ef16425f16ee23610f50d67a0a299dceb6195
Cracked-Walnut <maolud62@gmail.com>
2020-07-30 18:46:34 -0400

    Fixed player x velocity when transitioning between animations

--- Square-Up!/Assets/Scripts/Camera/CameraShake.cs
+++ Square-Up!/Assets/Scripts/Camera/CameraShake.cs
@@ -4,30 +4,14 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour {
 
-    public IEnumerator shake (float duration, float magnitude) {
-        
-        Vector3 originalPos = transform.localPosition;
+    [SerializeField] private Animator cameraAnimator;
 
-        float elapsed = 0.0f;
-
-        while (elapsed < duration) {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = new Vector3(x, y, originalPos.z);
-
-            elapsed += Time.deltaTime;
-
-            yield return null;
-            
-        }
-
-        transform.localPosition = originalPos;
+    public void camShake() {
+        cameraAnimator.SetTrigger("Shake");
     }
 
 }
 
 /*
 Sources:
-1) B., Brackeys, 'CAMERA SHAKE in Unity', 2018. [Online]. Available: https://www.youtube.com/watch?v=9A9yj8KnM8c [Accessed: 25-May-2020].
 */
