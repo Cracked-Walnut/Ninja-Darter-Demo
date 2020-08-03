5dae0c7783d4ef28844f52dda1691e4ed4140800
Cracked-Walnut <maolud62@gmail.com>
2020-08-02 20:03:29 -0400

    Level-1 populated with enemies

--- Square-Up!/Assets/Scripts/Weapon/Enemy Weapons/EnemyBow.cs
+++ Square-Up!/Assets/Scripts/Weapon/Enemy Weapons/EnemyBow.cs
@@ -6,15 +6,31 @@ public class EnemyBow : MonoBehaviour {
 
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject projectile;
+    [SerializeField] private string direction;
     // private float rateOfFire;
     private EnemyArrow enemyArrow;
+    private Quaternion accuracy;
 
     void Awake() {
         enemyArrow = FindObjectOfType<EnemyArrow>();
     }
 
     void shoot() {
-            Quaternion accuracy = Quaternion.Euler(0, 0, 0); // the direction of the projectile, left
+
+        switch(direction) { // firing direction
+            case "left":
+                accuracy = Quaternion.Euler(0, 0, 0);
+                break;
+
+            case "right":
+                accuracy = Quaternion.Euler(0, 0, 180);
+                break;
+            
+            default:
+                accuracy = Quaternion.Euler(0, 0, 0);
+                break;
+        }
+            
             Instantiate(projectile, firePoint.position, accuracy);
     }
 }
